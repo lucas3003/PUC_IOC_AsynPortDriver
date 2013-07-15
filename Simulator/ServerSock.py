e7a5ce51a1cee041283aeee643645c37e7e868d0
Lucas Jose <lucas_5639@hotmail.com>
2013-07-15 13:03:18 -0300

    Automation of simulator

--- Simulator/ServerSock.py
+++ Simulator/ServerSock.py
@@ -21,9 +21,10 @@ class ServerSock:
 			for i in range(0,4):		
 				print "Received on byte %d: %d " %(i, ord(data[i]))				
 					
-			print "End of data"	
+			print "End of data"
+			command = ord(data[2])			
 			
-			if self.operacao == "read":
+			if command == 0x10:		
 				#Make a packet with random 18 bits
 				payload1 = randint(0,3)
 				payload2 = randint(0, 255)
@@ -34,11 +35,11 @@ class ServerSock:
 				packet = [0, 20, 0x11, 3, payload1, payload2, payload3, checksum&0xFF]
 				
 			else: 
-				if self.operacao == "write" or self.operacao == "write_block_curve":
+				if command == 0x20 or command == 0x41:				
 					packet = [0, 20, 0xE0, 0, 12]  #Simulate a successful write
 			
 				else: 
-					if self.operacao == "read_block_curve":
+					if command == 0x40:					
 						packet = [0, 20, 0x41, 0xFF, 1, 4] 
 						for i in range(0,16384):
 							packet.append(randint(0, 255))
@@ -59,8 +60,7 @@ class ServerSock:
 			clientsock, addr = self.sock.accept()			
 			thread.start_new_thread(self.handler, (clientsock, addr))	
 			
-	def __init__(self, host, port):
-		self.operacao = raw_input("Operation(read, write, read_block_curve or write_block_curve):");
+	def __init__(self, host, port):		
 		self.HOST = host
 		self.PORT = port
 		self.BUFSIZ = 16391;
