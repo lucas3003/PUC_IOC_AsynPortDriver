eed024bd842dd71721ff10094eb0dff0e42af2ac
igor <igor.snunes@gmail.com>
2013-08-05 16:44:33 -0300

    Revert "Reformulacao e modularizacao completa do codigo"
    This reverts commit ce6d3fe3f7bc404d4df2129c5c8552e47bcb5ea1.

---
+++ PUCApp/src/O.linux-x86_64/PUC.dbd.d
@@ -0,0 +1,50 @@
+# DO NOT EDIT: This file created by mkmf.pl,v 1.5 2002/03/25 21:33:24 jba Exp $ 
+
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/base.dbd
+../O.Common/PUC.dbd : ../portInclude.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuGlobal.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuConvert.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/aaiRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/aaoRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/aiRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/aoRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/aSubRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/biRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/boRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/calcRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/calcoutRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/compressRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/dfanoutRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/eventRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/fanoutRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/longinRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/longoutRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/mbbiRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/mbbiDirectRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/mbboRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/mbboDirectRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/permissiveRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/selRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/seqRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/stateRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/stringinRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/stringoutRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/subRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/subArrayRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/waveformRecord.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/devSoft.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuAlarmSevr.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuAlarmStat.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuCompress.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuFtype.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuIvoa.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuOmsl.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuPini.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuPriority.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuScan.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuSimm.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/menuYesNo.dbd
+../O.Common/PUC.dbd : /home/igor/epics/base/dbd/dbCommon.dbd
+
+
+../O.Common/PUCInclude.dbd : ../Makefile
--- PUCApp/src/PortConnect.h
+++ PUCApp/src/PortConnect.h
@@ -1,24 +1,17 @@
 #ifndef PORT_CONNECT_H_
 #define PORT_CONNECT_H_
-#define PUC
+#define BPM
 
 #include <asynPortDriver.h>
-#include <Sllp.cpp>
-#include <Serial.cpp>
+#include <asynOctetSyncIO.h>
+#include <Command.cpp>
 
-#if defined BPM
-	#define P_TemperatureSetPoint "T_SetPoint"
-	#define P_TemperatureSensor1      "T_Sensor1"
-	#define P_TemperatureSensor2   "T_Sensor2"
-	#define P_TemperatureSensor3    "T_Sensor3"
-	#define P_TemperatureSensor4    "T_Sensor4"
-	#define P_SwitchState  "S_State"
-#elif defined PUC
-	#define P_AddressString "ADDRESS"
-	#define P_IdString      "ID"	
-#endif
-
-
+#define P_TemperatureSetPoint "T_SetPoint"
+#define P_TemperatureSensor1      "T_Sensor1"
+#define P_TemperatureSensor2   "T_Sensor2"
+#define P_TemperatureSensor3    "T_Sensor3"
+#define P_TemperatureSensor4    "T_Sensor4"
+#define P_SwitchState  "S_State"
 
 
 class PortConnect : public asynPortDriver
@@ -34,20 +27,18 @@ public:
     //virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value, size_t nElements, size_t *nIn);
     
 protected:
-#if defined BPM
+#ifdef BPM
 	int P_TemperatureSP;
 	int P_TemperatureS1;
 	int P_TemperatureS2;
 	int P_TemperatureS3;
 	int P_TemperatureS4;
 	int P_SState;
-#elif defined PUC
-	int P_Address;
-	int P_Id;
 #endif
 private:
-	Sllp sllp;
-	Serial serial;
+	Command com;
+	asynUser* user;
+	int timeout;
 };
 
 #endif
