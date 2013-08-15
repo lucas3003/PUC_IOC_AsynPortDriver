3bcd25f5fa3870328faf22722f7a3a1a16be2d9f
Lucas Jose <lucas_5639@hotmail.com>
2013-08-15 09:05:27 -0300

    Add Python simulators for PUC and BPM

---
+++ Simulator/PUC.py
@@ -0,0 +1,50 @@
+from ServerSock import ServerSock as Super
+from random import *
+
+class PUC(Super):
+
+	def __init__(self):
+		Super('127.0.0.1', 6543, self.handler)
+
+	def handler(self, clientsock, addr):
+		while 1:
+			data = clientsock.recv(16391)
+
+			if not data:
+				print "Nothing received"
+				break
+
+			for i in range(0,len(data)):
+				print"Received on byte %d: %d" %(i, ord(data[i]))
+
+			print "End of data"
+
+			command = ord(data[0])
+
+			if command == 0x02:
+				#Simulate "list var" command
+				packet = [0x03, 0x06, 0x83, 0x03, 0x03, 0x03, 0x03, 0x83]
+
+			if command == 0x04:
+				#Simulate "list group var" command
+				packet = [0x05, 0x00]
+
+			if command == 0x08:
+				#Simulate "list curves" command
+				packet = [0x09, 0x00]
+
+
+			if command == 0x10:
+				#Simulate response of 18 bits
+				packet = [0x11, 0x03, randint(0,3), randint(0,255), randint(0,255)]
+
+			if command == 0x20:
+				#Simulate a sucesfull write
+				packet = [0xE0, 0x00]
+
+			response = bytearray(packet)
+			clientsock.send(response)
+			print "Response send"
+	
+
+sock = PUC()
--- Simulator/ServerSock.py
+++ Simulator/ServerSock.py
@@ -2,71 +2,29 @@ from socket import *
 from array import *
 import thread
 import threading
-from random import *
+
 
 #Software who simulate the basic commands from PUC (Placa Universal de Controle, in portuguese)
 #Communication by ip and port specified on the last lines
 class ServerSock:
 	
-	def handler(self, clientsock, addr):
-		 while 1:			
-			data = clientsock.recv(self.BUFSIZ)
-
-			if not data:
-				print "Nothing received"
-				break
-			
-			print "Received: %s" %(data)
-							
-			for i in range(0,4):		
-				print "Received on byte %d: %d " %(i, ord(data[i]))				
-					
-			print "End of data"
-			command = ord(data[2])			
-			
-			if command == 0x10:		
-				#Make a packet with random 18 bits
-				payload1 = randint(0,3)
-				payload2 = randint(0, 255)
-				payload3 = randint(0,255)			
-				checksum = (payload1+payload2+payload3+20+0x11+3)				
-				checksum = checksum & 0xFF
-				checksum = 0x100 - checksum
-				packet = [0, 20, 0x11, 3, payload1, payload2, payload3, checksum&0xFF]
-				
-			else: 
-				if command == 0x20 or command == 0x41:				
-					packet = [0, 20, 0xE0, 0, 12]  #Simulate a successful write
-			
-				else: 
-					if command == 0x40:					
-						packet = [0, 20, 0x41, 0xFF, 1, 4] 
-						for i in range(0,16384):
-							packet.append(randint(0, 255))
-						packet.append(0)						
-				
-					else:
-						print "Invalid operation"
-			
-			print packet
-			
-			resposta = bytearray(packet)
-			clientsock.send(resposta)
-			print "Request send"
-	
 	def start(self):
 		while 1:
 			self.sock.listen(2)				
 			clientsock, addr = self.sock.accept()			
-			thread.start_new_thread(self.handler, (clientsock, addr))	
+			thread.start_new_thread(self.HANDLER, (clientsock, addr))	
 			
-	def __init__(self, host, port):		
+	def __init__(self, host, port, handler):		
 		self.HOST = host
 		self.PORT = port
-		self.BUFSIZ = 16391;
+		#self.BUFSIZ = 16391;
 		self.ADDR = (self.HOST, self.PORT)
 		self.sock = socket(AF_INET, SOCK_STREAM)
-		self.sock.bind(self.ADDR)		
+		self.sock.bind(self.ADDR)
+		self.HANDLER = handler
 
-sock = ServerSock('127.0.0.1', 6543)
-sock.start()
+		self.start()
+
+			
+
+
